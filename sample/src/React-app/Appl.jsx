b8214845b062d131c16cfcf96aba6935a463c16b
Shobika S <142974445+Shobikas24@users.noreply.github.com>
2023-12-16 15:01:44 +0530

    first commit

--- sample/src/React-app/Appl.jsx
+++ sample/src/React-app/Appl.jsx
@@ -1,42 +1,86 @@
-import React from 'react';
-import Slider from 'react-slick';
+// TestimonialCarousel.js
+import React, { useState, useEffect } from 'react';
+import './Css/A.css';
+import bird from './images/bird.PNG';
+import dog from './images/dog.png';
+import girl from './images/girl.png';
+import Background4 from './images/background4.jpg';
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+const testimonials = [
+  {
+    id: 1,
+    imagesrc:bird,
+    title: '$39',
+    subtitle:'start',
+    content: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu risus sapien pellentesque.',
+    button:'Buy Now',
+  },
+  {
+    id: 2,
+    imagesrc:dog,
+    title: '$59',
+    subtitle:'Basic',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu risus sapien pellentesque.',
+    button:'Buy Now',
+  },
+  {
+    id: 3,
+    imagesrc:girl,
+    title: '$89',
+    subtitle:'Pro',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu risus sapien pellentesque.',
+    button:'Buy Now',
+  },
+];
 
 const TestimonialCarousel = () => {
-  // Testimonial data (you can replace this with your actual data)
-  const testimonials = [
-    {
-      id: 1,
-      image: 'url_to_image_1.jpg',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum.',
-    },
-    {
-      id: 2,
-      image: 'url_to_image_2.jpg',
-      text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
-    },
-    // Add more testimonials as needed
-  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+    }, 5000); // Change slide every 5 seconds (adjust as needed)
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleIndicatorClick = (index) => {
+    setCurrentIndex(index);
   };
 
   return (
-    <Slider {...settings}>
-      {testimonials.map((testimonial) => (
-        <div key={testimonial.id} className="testimonial-card">
-          <img src={testimonial.image} alt={`Testimonial ${testimonial.id}`} />
-          <p>{testimonial.text}</p>
-        </div>
-      ))}
-    </Slider>
+    <div className="Section4 w-100 h-100 bg-no-repeat d-flex align-items-center justify-content-center"      style={{ backgroundImage: `url(${Background4})` }}
+    >
+    <div className="testimonial-carousel " style={{borderStyle:"dotted"}}>
+      <div className="row">
+        <h1></h1>
+      </div>
+      <div className="row">
+        {testimonials.map((testimonial, index) => (
+          <div key={testimonial.id} className="col" style={{borderStyle:"double"}}>
+            <h3>Ou</h3>
+            <div className={`card ${index === currentIndex ? 'active' : ''}`}>
+              <img src={testimonial.imagesrc} alt="img" className="img-thumbnail w-50"/>
+              <h2>{testimonial.title}</h2>
+              <h3>{testimonial.subtitle}</h3>
+              <p>{testimonial.content}</p>
+              <button>{testimonial.button}</button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="carousel-indicator">
+        {testimonials.map((_, index) => (
+          <div
+            key={index}
+            className={`indicator ${index === currentIndex ? 'active' : ''}`}
+            onClick={() => handleIndicatorClick(index)}
+          ></div>
+        ))}
+      </div>
+    </div>
+    </div>
   );
 };
 
--- sample/src/React-app/Section12.jsx
+++ sample/src/React-app/Section12.jsx
@@ -1,39 +1,70 @@
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
+
 import Background1 from "./images/background1.jpg";
 
-function GridBasicExample() {
-  return (
-    <div  className="w-100  bg-no-repeat h-100 d-flex align-items-center justify-content-center"
-    id="Section12"
-    style={{ backgroundImage: `url(${Background1})` }}>
-    <Form style={{borderStyle:"dotted",width:"80%",marginTop:"100px"}} className="form">
-      <Row>
-        <Col>
-          <Form.Control placeholder="Name" />
-        </Col>
-        <Col>
-        <Form.Control type="email" placeholder="Email" />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Control placeholder="Subject" />
-        </Col>
-        <Col>
-        <Form.Control type="number" placeholder="Phone" />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-        <Form.Control type="textarea" placeholder="Message" />
 
-        </Col>
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+
+function FormExample() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Control
+            required
+            type="text"
+            placeholder="Name"
+            defaultValue="Mark"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            required
+            type="email"
+            placeholder="email"
+            defaultValue="Otto"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
       </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="6" controlId="validationCustom03">
+          <Form.Control type="text" placeholder="Subject" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid subject.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="3" controlId="validationCustom04">
+          <Form.Label>State</Form.Label>
+          <Form.Control type="text" placeholder="State" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid state.
+          </Form.Control.Feedback>
+        </Form.Group>
+      
+      </Row>
+     
+      <Button type="submit">Send your message</Button>
     </Form>
-    </div>
   );
 }
 
-export default GridBasicExample;
+export default FormExample;
